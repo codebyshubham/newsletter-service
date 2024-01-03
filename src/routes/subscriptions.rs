f1a73849fbc86f7afbecc70fc1710863cae1bf8f
Shubham Patel <shubhampatel518@gmail.com>
2024-01-03 13:04:55 +0530

    gracefully handle duplicacy

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let find_user_result = sqlx::query!("SELECT email FROM subscriptions where email = $1", form.email)
+        .fetch_one(db_pool.get_ref())
+        .await;
+
+    if let Ok(find_user) = find_user_result {
+        if !find_user.email.is_empty() {
+            println!("User is already subscribed!");
+            return HttpResponse::BadRequest().body("User is already subscribed!");
+        }
+    }
+
     let result = sqlx::query!(
             r#"
             INSERT INTO subscriptions (id, email, name, subscribed_at)
